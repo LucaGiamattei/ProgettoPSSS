2e34165639c674f4743f9de99a203c6d4d4b9ac9
Jgio-Elli <giorgio.fari96@gmail.com>
2020-10-07 11:03:06 +0200

    fix

--- Codice/WebAppSmartLearning/src/dataLayer/videoroom/interfaces/API_VideoRoomDB.java
+++ Codice/WebAppSmartLearning/src/dataLayer/videoroom/interfaces/API_VideoRoomDB.java
@@ -1,6 +1,12 @@
 package dataLayer.videoroom.interfaces;
 
+import dataLayer.utilities.StateResult;
+import dataLayer.utilities.idFasciaOraria;
+import dataLayer.videoroom.entities.VideoRoomDB;
+
 public interface API_VideoRoomDB {
+	public StateResult createNewRoom(idFasciaOraria id, VideoRoomDB videoRoom);
+	public StateResult getRoom(idFasciaOraria id, VideoRoomDB videoRoom);
 	
 
 }
